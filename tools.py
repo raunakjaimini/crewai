116cd6d7709b9201096110a9772cc433d08bc858
Raunak Jaimini <76209436+raunakjaimini@users.noreply.github.com>
2024-08-20 13:18:29 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,6 +1,5 @@
 from crewai_tools import YoutubeChannelSearchTool
 
-# Initialize the tool with a specific Youtube channel handle to target your search
-# yt_tool = YoutubeChannelSearchTool(youtube_channel_handle='@krishnaik06')
-yt_tool = YoutubeChannelSearchTool(youtube_channel_handle='@valuebound7310')
+
+yt_tool = YoutubeChannelSearchTool(youtube_channel_handle='@krishnaik06')
 
