429e23db5161c973385b31febbac23e009bfed8c
Raunak Jaimini <76209436+raunakjaimini@users.noreply.github.com>
2024-08-20 12:47:26 +0530

    Add files via upload

---
+++ tools.py
@@ -0,0 +1,6 @@
+from crewai_tools import YoutubeChannelSearchTool
+
+# Initialize the tool with a specific Youtube channel handle to target your search
+# yt_tool = YoutubeChannelSearchTool(youtube_channel_handle='@krishnaik06')
+yt_tool = YoutubeChannelSearchTool(youtube_channel_handle='@valuebound7310')
+
